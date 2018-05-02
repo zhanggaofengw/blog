68b22e649f55714bef81d2bca17d40b4d2f60a15
张高峰 <1270782070@qq.com>
2018-05-02 14:13:01 +0800

    登录和注册简版

--- models/login.js
+++ models/login.js
@@ -1,9 +1,9 @@
-const {success, error} = require('./config')
+const {success, error} = require('./config');
 const crypto = require('crypto');
 const { createWebAPIRequest } = require('../util/util');
 const mongodb = require('./db');
-const express = require('express')
-const router = express()
+const express = require('express');
+const router = express();
 //读取用户信息
 router.get('/', (req, res) => {
     const name = req.query.name
@@ -22,12 +22,12 @@ router.get('/', (req, res) => {
     }
     createWebAPIRequest(
         '192.168.1.6',
-        '/register',
+        '/login',
         'POST',
         data,
         cookie,
         (req, cookie) => {
-            console.log(req)
+             console.log(req)
             res.set({
                 'Set-Cookie': cookie
             })
@@ -36,9 +36,9 @@ router.get('/', (req, res) => {
         err => res.status(502).send('fetch error')
     )
     //打开数据库
-    mongodb.open(function (err, db) {
+    mongodb.open((err, db) => {
         if (err) {
-            return res.send(err);//错误，返回 err 信息
+            return res.send({statueCode: error.code, msg: err});//错误，返回 err 信息
         }
         //读取 users 集合
         db.collection('users', function (err, collection) {
@@ -48,32 +48,21 @@ router.get('/', (req, res) => {
             }
             //查找用户名（name键）值为 name 一个文档
             collection.findOne({
-                name: name
+                name: req.query.name
             }, function (err, user) {
+                mongodb.close();
                 if (err) {
-                    mongodb.close();
                     return res.send({statueCode: error.code, msg: err});//失败！返回 err 信息
                 }
-                if (user) {
-                    mongodb.close();
-                    return res.send({statueCode: error.code, msg: '该用户已存在'})
-                } else {
-                    //将用户数据插入 users 集合
-                    collection.insert({
-                        name: name,
-                        password: password
-                    }, {
-                        safe: true
-                    }, function (err, user) {
-                        mongodb.close();
-                        if (err) {
-                            return res.send({statueCode: error.code, msg: err});//错误，返回 err 信息
-                        }
-                        res.send({statueCode: success.code, msg: '注册成功'});//成功！err 为 null，并返回存储后的用户文档
-                    });
+                if (!user) {
+                    res.send({statueCode: error.code, msg: '该用户不存在'});//失败！返回 err 信息
+                } else if(user.password === password){
+                    res.send({statueCode: success.code, msg: '登录成功'});//成功！返回查询的用户信息
+                } else if(user.password !== password){
+                    res.send({statueCode: success.code, msg: '用户名或密码错误'});
                 }
-            })
+            });
         });
-    });
+    })
 })
 module.exports = router
